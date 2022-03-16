9bdf8a03a89730dc32b4cf0d4e6e2665d6370736
KELLY LUU <kellyluuu@gmail.com>
2022-03-15 23:12:45 -0500

    adds title and message container

--- js/main.js
+++ js/main.js
@@ -1,13 +1,25 @@
-console.log("Js is up and running!")
-
 /*----- constants -----*/
 // X & O - Players
-// winning combinations
-
+const PLAYERS = {
+    '1': 'X',
+    '-1': 'O,',
+}
+// winning combinations 012 345 678
+const COMBOS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,2,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+]
 /*----- app's state (variables) -----*/
 // where the users click (where to put an x or o) - make sure a square is not taken first
-// Whose turn is it
-// when we have a winner or if there is a tie - if game is ongoing
+let board; // a data variable that stores the board position 
+let turn; // will be X or O 
+let winner; // this can be 3 things - X/O, tie, or null
 // Bonus - score for games won
 // Bonus - if the player wants to quit
 // Bonus - timer for game turn
@@ -28,13 +40,3 @@ console.log("Js is up and running!")
 // check for 3 in a row - or winner 
 // render messages to the dom 
 
-
-// 1) Define required constants
-// 2) Define required variables used to track the state of the game
-// 3) Store elements on the page that will be accessed in code more than once in variables to make code more concise, readable and performant.
-// 4) Upon loading the app should:
-//     4.1) Initialize the state variables
-//     4.2) Render those values to the page
-//     4.3) Wait for the user to click a square
-// 5) Handle a player clicking a square
-// 6) Handle a player clicking the replay button
