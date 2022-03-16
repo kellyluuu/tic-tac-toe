b7ee436e592a0492004f923f8d45d9f0745fa97f
KELLY LUU <kellyluuu@gmail.com>
2022-03-16 01:17:28 -0500

    update to working tic tac toe

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 // X & O - Players
 const PLAYERS = {
     '1': 'X',
-    '-1': 'O,',
+    '-1': 'O',
 }
 // winning combinations 012 345 678
 const COMBOS = [
@@ -45,7 +45,68 @@ resetButton.addEventListener('click', init)
 
 /*----- functions -----*/
 // initialize game - Start
-// handle user interation - handleMove()
-// check for 3 in a row - or winner 
-// render messages to the dom 
+function init(){
+    console.log('init function called')
+    board = new Array(9).fill(null) // array to keep track of player move
+    turn = 1; // player 1 goes first which is X
+    winner = null; // set initial winner to no one 
+    render();
+}   
 
+init(); //if you want to start the game when the page load 
+
+
+
+// handle user interation - handleMove()
+function handleMove (event){
+    const squareNumber = parseInt(event.target.dataset.square);
+    if(board[squareNumber] || winner ){
+        return //end the turn 
+    }
+    board[squareNumber] = turn; //set a index and value in board array
+    winner = checkForWinner()
+    turn *= -1; // switched the turn 
+    render()
+}
+
+
+// check for 3 in a row - or winner 
+function checkForWinner(){
+    //this can be 3 things x/0, tie, or null 
+    for (let index in COMBOS) {
+        if (
+          board[COMBOS[index][0]] == turn &&
+          board[COMBOS[index][1]] == turn &&
+          board[COMBOS[index][2]] == turn 
+        ) {
+          return turn
+        }
+      }
+    
+      if (board.includes(null)) {
+        return null
+      }
+    
+      return "tie"
+    }
+
+// render messages to the dom
+function render() {
+    // console.log('render function called')
+    // puts an x or o on the board mapped from board
+    board.forEach(function (value, index) {
+      domSquare[index].textContent = PLAYERS[value]
+    })
+  
+    if (!winner) {
+      // tell whose turn it is
+      domMessage.textContent = `${PLAYERS[turn]}'s turn`
+    } else if (winner === "tie"){
+      // tell the user there is a tie
+      domMessage.textContent = "Tie game!"
+    } else {
+      // tell them the winner
+      domMessage.textContent = `${PLAYERS[winner]} Wins!`
+    }
+  
+  }
