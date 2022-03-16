481622d62940cfcb8305f20b2a603d1ea7974685
KELLY LUU <kellyluuu@gmail.com>
2022-03-15 22:32:27 -0500

    initial

---
+++ js/main.js
@@ -0,0 +1,16 @@
+/*----- constants -----*/
+// X & 0 - Players
+// winning combination 
+
+/*----- app's state (variables) -----*/
+// where they click - where to put x and o 
+// who's turn it is 
+// BONUS - add score for games won
+// BONUS - if play wants to quit
+// BONUS - timer
+
+/*----- cached element references -----*/
+// the game squares
+// reset
+/*----- event listeners -----*/
+/*----- functions -----*/
