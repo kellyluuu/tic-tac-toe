40d7c2b1537ec9614182568b00b6b89884f16706
KELLY LUU <kellyluuu@gmail.com>
2022-03-15 22:53:35 -0500

    add psudo code for game

--- js/main.js
+++ js/main.js
@@ -1,16 +1,40 @@
+console.log("Js is up and running!")
+
 /*----- constants -----*/
-// X & 0 - Players
-// winning combination 
+// X & O - Players
+// winning combinations
 
 /*----- app's state (variables) -----*/
-// where they click - where to put x and o 
-// who's turn it is 
-// BONUS - add score for games won
-// BONUS - if play wants to quit
-// BONUS - timer
+// where the users click (where to put an x or o) - make sure a square is not taken first
+// Whose turn is it
+// when we have a winner or if there is a tie - if game is ongoing
+// Bonus - score for games won
+// Bonus - if the player wants to quit
+// Bonus - timer for game turn
 
 /*----- cached element references -----*/
-// the game squares
-// reset
+// message container - h1
+// the game squares 
+// reset button
+
+
 /*----- event listeners -----*/
+// mouse click on 9 squares
+// mouse click on reset 
+
 /*----- functions -----*/
+// initialize game - Start
+// handle user interation
+// check for 3 in a row - or winner 
+// render messages to the dom 
+
+
+// 1) Define required constants
+// 2) Define required variables used to track the state of the game
+// 3) Store elements on the page that will be accessed in code more than once in variables to make code more concise, readable and performant.
+// 4) Upon loading the app should:
+//     4.1) Initialize the state variables
+//     4.2) Render those values to the page
+//     4.3) Wait for the user to click a square
+// 5) Handle a player clicking a square
+// 6) Handle a player clicking the replay button
